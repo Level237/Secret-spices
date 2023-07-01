08f495fa4282ff49fc86c2f3c9b149f59eeef366
frankzohim <delanofofe@gmail.com>
2023-07-01 14:44:33 +0100

    commit from monaco

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,57 @@
+			<!-- FOOTER-3
+			============================================= -->
+			<footer id="footer-3" class="footer division">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+
+
+							<!-- FOOTER LOGO -->	
+							<div class="footer-logo"><img src="images/logo-02.png" alt="footer-logo"></div>
+
+							<!-- FOOTER SOCIAL LINKS -->
+							<div class="footer-socials-links">
+
+								<!-- List -->
+								<ul class="foo-socials text-center clearfix">																	
+									<li><a href="#" class="ico-facebook"><i class="fab fa-facebook-f"></i></a></li>
+									<li><a href="#" class="ico-twitter"><i class="fab fa-twitter"></i></a></li>	
+									<li><a href="#" class="ico-instagram"><i class="fab fa-instagram"></i></a></li>
+									<li><a href="#" class="ico-youtube"><i class="fab fa-youtube"></i></a></li>
+									<li><a href="#" class="ico-pinterest"><i class="fab fa-pinterest-p"></i></a></li>
+														
+									<!--
+									<li><a href="#" class="ico-dribbble"><i class="fab fa-dribbble"></i></a></li>		
+									<li><a href="#" class="ico-behance"><i class="fab fa-behance"></i></a></li>										
+									<li><a href="#" class="ico-linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+									<li><a href="#" class="ico-tumblr"><i class="fab fa-tumblr"></i></a></li>	
+									<li><a href="#" class="ico-google-plus"><i class="fab fa-google-plus-g"></i></a></li>							
+									<li><a href="#" class="ico-vk"><i class="fab fa-vk"></i></a></li>-->
+								</ul>
+															
+							</div>
+
+
+							<!-- FOOTER LINKS -->
+							<div class="footer-links">
+								<ul class="bottom-footer-list clearfix">	
+									<li><p><a href="#">About</a></p></li>
+									<li><p><a href="#">Menu</a></p></li>
+									<li><p><a href="#">Order Now</a></p></li>
+									<li><p><a href="#">Gallery</a></p></li>
+									<li><p><a href="#">Gift Cards</a></p></li>
+									<li><p class="last-li"><a href="#">Contacts</a></p></li>
+								</ul>
+							</div>
+
+
+							<!-- FOOTER COPYRIGHT -->
+							<div class="footer-copyright">
+								<p>&copy; 2020 Testo. All Rights Reserved</p>
+							</div>	
+
+
+						</div>	
+					</div>     <!-- End row -->
+				</div>	   <!-- End container -->										
+			</footer>	<!-- END FOOTER-3 -->
