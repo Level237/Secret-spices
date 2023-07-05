fd9d5112439ec5bcbb15d49e7e186ee5d7a3a5a8
Frank NYAWA <FRANK NYAWA@KSOFT.LOCAL>
2023-07-05 17:16:42 +0100

    menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,13 +11,13 @@
 					<!-- CALL BUTTON -->
 				    <div class="callusbtn"><a href="tel:123456789"><i class="fas fa-phone"></i></a></div>
 
-					
+
 					<!-- LOGO IMAGE -->
-	                <div class="logo" data-mobile-logo="images/logo-02.png" data-sticky-logo="images/logo-02.png">
-	                	<a href="#hero-6"><img src="images/logo-02.png" alt="header-logo"></a>
+	                <div class="logo" data-mobile-logo="images/logo secret bg.png" data-sticky-logo="images/logo secret bg.png">
+	                	<a href="#hero-6"><img src="images/logo secret bg.png" alt="header-logo"></a>
 					</div>
 
-					
+
 					<!-- BURGER MENU -->
 					<div class="burger-menu">
 						<div class="line-menu line-half first-line"></div>
@@ -28,214 +28,25 @@
 
 					<!-- MAIN MENU -->
 	                <nav class="navik-menu menu-caret navik-yellow">
-	                	<ul class="top-list">
+
+                        <ul class="top-list">
 
 	                		<!-- DROPDOWN MENU -->
-	                		<li><a href="#">About</a>
-	                			<ul>
-	                            	<li><a href="about.html">About Testo</a></li>
-	                            	<li><a href="team.html">Meet The Team</a></li>
-									<li><a href="gallery.html">Images Gallery</a></li>
-									<li><a href="gift-cards.html">Gift Cards</a></li>
-									<li><a href="faqs.html">F.A.Q.s</a></li>
-									<li><a href="terms.html">Terms & Privacy</a></li>
-	                            </ul>
-	                		</li>
+                            <li>
+                                <a href="#">Accueil</a>
+                            </li>
+                            <li>
+                                <a href="#">Produits</a>
+                            </li>
+                            <li>
+                                <a href="#">Engagements</a>
+                            </li>
+                            <li>
+                                <a href="#">Recettes</a>
+                            </li>
+                        </ul>
 
-	                		<!-- DROPDOWN MENU -->
-	                    	<li><a href="#">Our Menu</a>
-	                        	<ul>
-	                            	<li><a href="menu-1.html">Menu v.1 - Tabs Cards</a></li>
-									<li><a href="menu-2.html">Menu v.2 - Cards</a></li>
-									<li><a href="menu-3.html">Menu v.3 - Sections</a></li>
-	                            </ul>
-	                        </li>
 
-	                        <!-- MEGA MENU -->
-	                        <li class="mega-menu"><a href="#">Megamenu</a>
-								<ul>
-									<li>
-
-										<!-- Mega menu container -->
-										<div class="mega-menu-container">
-											<div class="row">
-
-												<!-- Column -->
-												<div class="col-md-6 col-lg-4">
-
-													<div class="mega-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-05.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Aliquam and lobortis...</a></h4>
-																<div class="mega-menu-desc">
-																	Donec nec faucibus lobortis viverra blandit sem justo
-																</div>
-															</div>
-														</div>
-													</div>
-
-													<div class="mega-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-01.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Donec neca faucibus...</a></h4>
-																<div class="mega-menu-desc">
-																	Aliquam ultrices ullamcoroe ultrices gravida dictum
-																</div>
-															</div>
-														</div>
-													</div>
-
-													<div class="mega-menu-box last-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-07.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Lobortis sem mauris...</a></h4>
-																<div class="mega-menu-desc">
-																	Integer aliquet magna neca tellus orci quis semper
-																</div>
-															</div>
-														</div>
-													</div>
-
-												</div>
-
-												<!-- Column -->
-												<div class="col-md-6 col-lg-4">
-
-													<div class="mega-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-08.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Fusce semper aliquet...</a></h4>
-																<div class="mega-menu-desc">
-																	Laoreet tellus a consectetur bibendum dolor posuere
-																</div>
-															</div>
-														</div>
-													</div>
-
-													<div class="mega-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-09.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Aliquam libero ipsum...</a></h4>
-																<div class="mega-menu-desc">
-																	Sollicitudin ut gravida libero commodo sit malesuada
-																</div>
-															</div>
-														</div>
-													</div>
-
-													<div class="mega-menu-box last-menu-box">
-														<div class="mega-menu-media">
-															<div class="mega-menu-media-img">
-																<div class="mega-menu-thumbnail">
-																	<a href="blog-listing.html">
-																		<img src="images/img-10.jpg" alt="thumbnail">
-																	</a>
-																</div>
-															</div>
-															<div class="mega-menu-media-info">
-																<h4 class="mega-menu-heading"><a href="blog-listing.html">Posuere himenaeos...</a></h4>
-																<div class="mega-menu-desc">
-																	Porta gravida hendrerit vitae blandit in phasellus ipsum
-																</div>
-															</div>
-														</div>
-													</div>
-
-												</div>
-
-												<!-- Column -->
-												<div class="col-lg-4">
-
-													<div class="mega-menu-box last-menu-box">
-														<div class="mega-menu-thumbnail">
-															<a href="blog-listing.html">
-																<img src="images/img-26.jpg" alt="thumbnail">
-															</a>
-														</div>
-														<h4 class="mega-menu-heading"><a href="blog-listing.html">Etiam semper mauris...</a></h4>
-														<div class="mega-menu-desc">
-															Aliquam a augue suscipit, luctus neque purus ipsum neque undo dolor primis libero 
-															tempus, blandit a cursus varius magna
-														</div>
-													</div>
-
-												</div>
-
-											</div>
-										</div>
-
-									</li>
-								</ul>
-							</li>	<!-- END MEGA MENU -->
-
-	                        <!-- DROPDOWN MENU -->
-	                		<li><a href="#">Shop</a>
-	                			<ul>
-	                            	<li><a href="product-single.html">Single Product</a></li>
-									<li><a href="cart.html">Shopping Cart</a></li>
-	                            </ul>
-	                		</li>
-
-							<!-- DROPDOWN MENU -->
-							<li><a href="#">Blog</a>
-								<ul>
-	                            	<li><a href="blog-listing.html">Blog Listing</a></li>
-									<li><a href="single-post.html">Single Post</a></li>
-	                            </ul>
-							</li>
-
-	                        <!-- DROPDOWN MENU -->
-							<li><a href="#">Contacts</a>
-								<ul>
-		                        	<li><a href="booking.html">Book A Table</a></li>
-		                        	<li><a href="locations.html">Our Locations</a></li>
-									<li><a href="contacts.html">Contact Us</a></li>
-								</ul>
-							</li>
-
-							<!-- BASKET ICON -->
-							<li class="basket-ico ico-30">
-					        	<a href="cart.html">
-					        		<span class="ico-holder"><span class="flaticon-shopping-bag"></span> <em class="roundpoint">2</em></span>
-					        	</a>
-					        </li>
-
-	                    </ul>
 	                </nav>	<!-- END MAIN MENU -->
 
 
@@ -245,12 +56,12 @@
 			</div>     <!-- End container -->
 		</header>	<!-- END HEADER-3 -->
 
-        
+
 			<!-- HERO-6
-			============================================= -->	
+			============================================= -->
 			<section id="hero-6" class="hero-section division">
 
-				
+
 				<!-- SLIDER -->
 				<div class="slider mt-1001">
 			    	<ul class="slides">
@@ -270,8 +81,8 @@
 		       								<div class="caption-txt white-color">
 
 						       					<!-- Title -->
-								         	 	<h2>Farmers Special</h2>
-								         	 	<h3>From <span class="meat-color">$26.49</span></h3>	
+								         	 	<h2>Faciliter la cuisine </h2>
+								         	 	<h4>avec des assaisonements deja prets <span class="meat-color">$26.49</span></h4>
 
 											</div>
 										</div>
@@ -297,7 +108,7 @@
 
 						       					<!-- Title -->
 								         	 	<h2>Chicken Ranchero</h2>
-								         	 	<h3>From <span class="meat-color">$24.99</span></h3>	
+								         	 	<h3>From <span class="meat-color">$24.99</span></h3>
 
 											</div>
 										</div>
@@ -323,7 +134,7 @@
 
 						       			 		<!-- Title -->
 								         	 	<h2>Quattro Formaggi</h2>
-								         	 	<h3>From <span class="meat-color">$32.25</span></h3>	
+								         	 	<h3>From <span class="meat-color">$32.25</span></h3>
 
 											</div>
 					        			</div>
@@ -336,5 +147,5 @@
 				    </ul>
 			  	</div>	<!-- END SLIDER -->
 
-				
-			</section>	<!-- END HERO-6 -->	
+
+			</section>	<!-- END HERO-6 -->
