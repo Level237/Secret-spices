5d56b82f40ec88724331ea6de8dc77c7578dd421
frankzohim <delanofofe@gmail.com>
2023-07-07 18:12:32 +0100

    commit from monaco

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,27 +1,36 @@
 		<!-- HEADER-3
 		============================================= -->
 
-            <div class="container-fluid">
+            <div class="container-fluid mt-5" id="header-desktop">
                 <div class="row">
-                    <div class="col-md-2">
+                    <div class="col-md-1">
                         <div >
                             <a href=""><img class="log" src="images/logo Secret-01.png" alt="header-logo"></a>
                         </div>
                     </div>
 
-                    <div class="col-md-10">
-                        <div class="d-flex flex-row-reverse bd-highlight">
+                    <div class="col-md-11">
+                        <div class="d-md-flex flex-row-reverse bd-highlight">
                             <div class="p-2 bd-highlight">
                                 <img class="lang" src="images/france.png" alt="">
                             </div>
 
                             <div class="p-2 bd-highlight">
                                 <div>
-                                    <form class="form" action="">
-                                        <input type="Search">
-                                        <button class="btn btn-light"><i class="fas fa-search"></i></button>
-                                        
-                                    </form>
+                                    <div class="middle-section">
+                                        <input class="search-bar" type="text" placeholder="Search"> 
+
+                                        <button class="search-button">
+
+                                            <img class="search-icon" src="images/icons/search.svg" alt="">
+                                            
+                                            <div class="tooltip">
+                                                Search
+                                            </div>
+
+                                        </button>
+
+                                    </div>
                                 </div>
                             </div>
                             <div class="p-2 bd-highlight">Contactez nous</div>
@@ -51,10 +60,75 @@
 
 
 
+<!-- HEADER-3
+		============================================= -->
+		<header id="header-3" class="header navik-header header-transparent header-shadow">
+			<div class="container">
 
-            </div>
 
-            </div>
+				<!-- NAVIGATION MENU -->
+				<div class="navik-header-container">
 
-        </div>
 
+					<!-- CALL BUTTON -->
+				    <div class="callusbtn"><a href="tel:123456789"><i class="fas fa-phone"></i></a></div>
+
+					
+					<!-- LOGO IMAGE -->
+	                <div class="logo" data-mobile-logo="images/logos/logo-one.png" data-sticky-logo="images/logos/logo-one.png">
+	                	<a href="#hero-6"><img src="images/logos/logo-one.png" alt="header-logo"></a>
+					</div>
+
+					
+					<!-- BURGER MENU -->
+					<div class="burger-menu">
+						<div class="line-menu line-half first-line"></div>
+						<div class="line-menu"></div>
+						<div class="line-menu line-half last-line"></div>
+					</div>
+
+
+					<!-- MAIN MENU -->
+	                <nav class="navik-menu menu-caret navik-yellow">
+	                	<ul class="top-list">
+
+	                		<!-- DROPDOWN MENU -->
+	                		<li><a href="#">Nos Produits</a>
+	                			
+	                		</li>
+
+	                		<!-- DROPDOWN MENU -->
+	                    	<li><a href="#">Nos Recettes</a>
+	                        	
+	                        </li>
+
+	                        <!-- MEGA MENU -->
+	                        <li class="mega-menu"><a href="#">Notre Engagement</a>
+								
+							</li>	<!-- END MEGA MENU -->
+
+	                        
+
+							<!-- DROPDOWN MENU -->
+							<li><a href="#">Blog</a>
+								
+							</li>
+
+                            <!-- DROPDOWN MENU -->
+	                		<li><a href="#">Contactez-Nous</a>
+	                			<ul>
+		                        	<li><a href="#">Book A Table</a></li>
+		                        	<li><a href="#">Our Locations</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+	                		</li>
+
+	                    </ul>
+	                </nav>	<!-- END MAIN MENU -->
+
+
+				</div>	<!-- END NAVIGATION MENU -->
+
+
+			</div>     <!-- End container -->
+		</header>	<!-- END HEADER-3 -->
