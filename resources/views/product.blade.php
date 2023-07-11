76e417930f0d6a7fe48202db519cfad7c6ed1d0e
frankzohim <delanofofe@gmail.com>
2023-07-11 15:34:30 +0100

    commit from monaco

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -14,7 +14,7 @@
         </div>
     </div>
   
-    <div class="row" style="margin-top:20px;">
+    <div class="row mb-5" style="margin-top:30px;">
         
         <div class="col d-flex flex-column product-row">
             
@@ -52,7 +52,7 @@
          <div class="col d-flex flex-column product-row">
             
             <div class="p-2 align-self-center">
-                <img src="{{ asset('images/products/secret-viande.png')}}" width="250px" alt="">
+                <img src="{{ asset('images/products/secret-poulet.png')}}" width="250px" alt="">
             </div>
 
             <div class="p-2 secret-spices">
@@ -85,7 +85,234 @@
          <div class="col d-flex flex-column product-row">
             
             <div class="p-2 fs-4 fw-normal align-self-center">
-                <img src="{{ asset('images/products/secret-viande.png')}}" width="250px" alt="">
+                <img src="{{ asset('images/products/secret-poisson.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret<br> Epices Poisson
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2  d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div style="margin-bottom: 50px;">
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm3 center-gamut">
+            <i class="fas fa-angle-right fs-4" style="color: #c70609"> 250 Gr</i>
+        </div>
+    </div>
+
+
+    <div class="row" style="margin-top:30px;">
+        
+        <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 fs-4 fw-normal align-self-center">
+                <img src="{{ asset('images/products/simulation-viande.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret <br>Epices  Viande
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2 fs-4 fw-normal d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+         <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 align-self-center">
+                <img src="{{ asset('images/products/simulation-poulet.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret<br> Epices Poulet
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2 fs-4 fw-normal d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+         <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 fs-4 fw-normal align-self-center">
+                <img src="{{ asset('images/products/simulation-poisson.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret<br> Epices Poisson
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2  d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+    <div style="margin-bottom: 50px;">
+
+    </div>
+
+    <div class="row">
+        <div class="col-sm3 center-gamut">
+            <i class="fas fa-angle-right fs-4" style="color: #c70609"> 450 Gr</i>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top:30px;">
+        
+        <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 fs-4 fw-normal align-self-center">
+                <img src="{{ asset('images/products/pot-viande.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret <br>Epices  Viande
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2 fs-4 fw-normal d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+         <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 align-self-center">
+                <img src="{{ asset('images/products/pot-viande.png')}}" width="250px" alt="">
+            </div>
+
+            <div class="p-2 secret-spices">
+                Secret<br> Epices Poulet
+                <div class="horizontal-mobile-line-gray" ></div>
+            </div>
+
+            <div class="p-2 fs-4 fw-normal d-flex justify-content-between">
+                
+                <div class="p-2">
+                    <img src="{{ asset('images/logos/logo-two.png')}}" width="150px" alt="">
+                </div>
+
+                <div class="p-2">
+                     <!-- Rating -->
+                    <div class="review-rating">
+                        <div class="stars-rating">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+         <div class="col d-flex flex-column product-row">
+            
+            <div class="p-2 fs-4 fw-normal align-self-center">
+                <img src="{{ asset('images/products/pot-poulet.png')}}" width="250px" alt="">
             </div>
 
             <div class="p-2 secret-spices">
