64a5f2159eb2b40a02b386dbe32b37bc5c6fa047
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-11 13:05:33 +0100

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,51 +1,62 @@
 			<!-- FOOTER-3
 			============================================= -->
-			<footer id="footer-3" class="footer-division">
-				
-                
-                <div class="d-md-flex flex-row justify-content-around">
-                    <div class="p-2">
-                         <div class="d-flex flex-column">
-                            <div class="p-2"> 
-                                <img src="images/logos/logo-one.png" width="354px" alt="footer-logo">
-                            </div>
-                            <div class="d-flex flex-column align-items-center">
-                                <div class="p-2">
-                                <p class="fs-4 fw-bold">L'alliee de la femme <br> ambitieuse et belle</p>
-                            </div>
-                            <div class="p-2">
-                                <a href="#" class="ico-facebook text-white"><i class="fab  fa-facebook-f"></i></a>
-                                 <a href="#" class="ico-instagram"><i class="fab fa-instagram"></i></a>
-                            </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                    <div class="align-self-center p-2">
-                        <div class="d-flex flex-column ">
-                            <div class="p-2 fs-4 fw-normal"><i class="fas fa-angle-right"></i> A Propos</div>
-                            <div class="p-2 fs-4 fw-normal"><i class="fas fa-angle-right"></i> Mentions légales</div>
-                            <div class="p-2 fs-4 fw-normal"><i class="fas fa-angle-right"></i> Données personnelles</div>
-                        </div>
-                    </div>
-                    <div class="align-self-center p-2">
-                        <div class="d-flex flex-column">
-                            <div class="p-2 fs-4 fw-normal"><i class="fas fa-angle-right"></i> Mentions sanitaires</div>
-                            <div class="p-2 fs-4 fw-normal"><i class="fas fa-angle-right"></i> Nous contacter</div>
-                        </div></div>
-                </div>
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="blocfooter">
-                            <div class="col-md-12">
-                                <!-- FOOTER COPYRIGHT -->
-                                <div class="footer-copyright fs-4 fw-bold">
-                                    <p> Copyright © 2023 Secret Spices. All rights reserved. Powered by K-SOFT TECHNOLOGIES </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+			<footer id="footer-3" class="footer-division footer-bg">
 
-                </div>     <!-- End row -->
-				</div>	   <!-- End container -->
-			</footer>	<!-- END FOOTER-3 -->
+
+			    <div class="d-md-flex flex-row justify-content-around">
+			        <div class="p-2">
+			            <div class="d-flex  flex-column">
+			                <div class="p-2">
+			                    <img src="images/logos/logo-one.png" width="200px" alt="footer-logo">
+			                </div>
+			                <div class="d-flex flex-column align-items-center">
+			                    <div class="">
+			                        <p class="fs-5 fw-bold">L'alliee de la femme <br> ambitieuse et belle</p>
+			                    </div>
+
+			                </div>
+
+			            </div>
+			        </div>
+			        <div class="align-self-center p-2">
+			            <div class="d-flex flex-column ">
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> A Propos</div>
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> Mentions légales</div>
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> Données personnelles</div>
+			            </div>
+			        </div>
+			        <div class="align-self-center p-2">
+			            <div class="d-flex flex-column">
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> Mentions sanitaires</div>
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> Evenements</div>
+			                <div class="p-2 fs-5 fw-normal"><i class="fas fa-angle-right"></i> Nous contacter</div>
+			            </div>
+			        </div>
+
+			    </div>
+
+			    <div class="row">
+			        <div class="col">
+
+			        </div>
+			        <div class="col">
+			            <a href="#" class="ico-facebook text-white"><i class="fab  fa-facebook-f"></i></a>
+			            <a href="#" class="ico-instagram"><i class="fab fa-instagram"></i></a>
+			        </div>
+			    </div>
+
+			    <div class="container-fluid">
+			        <div class="row">
+			            <div class="blocfooter">
+			                <div class="col-md-12">
+			                    <!-- FOOTER COPYRIGHT -->
+			                    <div class="footer-copyright fs-4 fw-bold">
+			                        <p> Copyright © 2023 Secret Spices. All rights reserved. Powered by K-SOFT TECHNOLOGIES </p>
+			                    </div>
+			                </div>
+			            </div>
+			        </div>
+
+			    </div> <!-- End row -->
+			    </div> <!-- End container -->
+			</footer> <!-- END FOOTER-3 -->
