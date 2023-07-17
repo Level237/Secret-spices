d487992bf8f3edfc16454d38fed00a4177b61d1b
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-17 15:37:19 +0100

    relation beetween role and user:Relation One To One

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'type'
+    ];
+
+    public function users():HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
