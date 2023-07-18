009e7cc70b9d5a1a130a49dbab673906b1684665
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-18 11:22:24 +0100

    update

---
+++ app/Http/Controllers/WeightController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\WeightRequest;
+use App\Models\Weight;
+use Illuminate\Http\Request;
+
+class WeightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.gammes.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.gammes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(WeightRequest $request)
+    {
+        $weight=new Weight;
+        $weight->weight_name=$request->weight_name;
+
+        return to_route('weight.create');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $weight=Weight::find($id);
+
+        return view('admin.gammes.edit',compact('weight'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
