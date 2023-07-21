6f123578f03bd7c5d275282457098635c6f24246
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-21 13:50:40 +0100

    update

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -352,11 +352,11 @@
             <li class="dropdown">
                 <a class="nav-link dropdown-toggle arrow-none nav-user px-2" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                     <span class="account-user-avatar">
-                        <img src="{{ asset('admin/assets/images/users/avatar-1.jpg') }}" alt="user-image" width="32" class="rounded-circle">
+                        <img src="{{ asset('images/logo secret bg.png') }}" alt="user-image" width="32" class="rounded-circle">
                     </span>
                     <span class="d-lg-flex flex-column gap-1 d-none">
                         <h5 class="my-0">{{ auth()->user()->name }}</h5>
-                        <h6 class="my-0 fw-normal">Founder</h6>
+                        <h6 class="my-0 fw-normal">Admin</h6>
                     </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end dropdown-menu-animated profile-dropdown">
