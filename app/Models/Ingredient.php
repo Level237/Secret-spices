ebd8e7327a8afb4ce9aa369860bc747ef16f5729
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-25 11:05:51 +0100

    update

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingredient extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'ingredient_name'
+    ];
+
+    public function recipes():BelongsToMany{
+        return $this->belongsToMany(Recipe::class);
+    }
 }
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,11 +2,30 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\Ingredient;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Recipe extends Model
 {
     use HasFactory;
 
+    protected $fillable=[
+        'name_recipe',
+        'category_id',
+        'description_recipe',
+        'time',
+        'number_person'
+    ];
+
+    public function category():BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    public function ingredients():BelongsToMany{
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
