b64abb8852e392086ecf91101c1c3cb86a9344dc
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-07-28 12:43:54 +0100

    update

--- resources/views/Recipes/detailCategory.blade.php
+++ resources/views/Recipes/detailCategory.blade.php
@@ -86,26 +86,7 @@ Categorie : {{ $category->category_name }}
 
 <!-- PAGE PAGINATION
 			============================================= -->
-<div class="bg-color-01 page-pagination division">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
 
-                <nav aria-label="Page navigation">
-                    <ul class="pagination justify-content-center">
-                        <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1"><i class="fas fa-angle-left"></i></a></li>
-                        <li class="page-item active"><a class="page-link" href="#">1 <span class="sr-only">(current)</span></a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item"><a class="page-link" href="#">4</a></li>
-                        <li class="page-item"><a class="page-link" href="#"><i class="fas fa-angle-right"></i></a></li>
-                    </ul>
-                </nav>
-
-            </div>
-        </div> <!-- End row -->
-    </div> <!-- End container -->
-</div> <!-- END PAGE PAGINATION -->
 </div>
 </div>
 
