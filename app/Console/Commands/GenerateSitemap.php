a6f914cb9e68de70e43736b870074955a82a3189
Balep Martin <84333489+Level237@users.noreply.github.com>
2023-08-04 11:37:20 +0100

    first sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use Spatie\Sitemap\Tags\Url;
 use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
 
 class GenerateSitemap extends Command
 {
@@ -11,20 +13,40 @@ class GenerateSitemap extends Command
      *
      * @var string
      */
-    protected $signature = 'app:generate-sitemap';
+    protected $signature = 'sitemap:generate';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Automatically Generate an XML Sitemap';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        SitemapGenerator::create('https://secret-spices.net')
+            ->getSitemap()
+
+            ->add(Url::create('/')->setPriority(1))
+            ->add(Url::create('/produits')->setPriority(0.9))
+            ->add(Url::create('/recettes')->setPriority(0.8))
+            ->add(Url::create('/recipes-videos')->setPriority(0.8))
+            ->add(Url::create('/engagements')->setPriority(0.8))
+            ->add(Url::create('/about')->setPriority(0.8))
+            ->add(Url::create('/contact')->setPriority(0.8))
+            ->add(Url::create('/galery')->setPriority(0.8))
+            ->add(Url::create('/evenements')->setPriority(0.8))
+            ->add(Url::create('/legal')->setPriority(0.5))
+            ->add(Url::create('/health-information')->setPriority(0.5))
+            ->add(Url::create('/personnal-data')->setPriority(0.5))
+            //here we have added four links, but you can add as many as you'd like
+
+            //location where you want to generate your sitemap file
+            ->writeToFile(public_path('sitemap.xml'));
+
+        return 'Sitemap generated';
     }
 }
